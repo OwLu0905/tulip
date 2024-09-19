fd176383c90f9608d24fb34fae49c0771d7094c6
OwLu0905 <owlu0905@gmail.com>
2024-09-19 22:38:32 +0800

    feat: add tagName argv

--- script/createTemplate.js
+++ script/createTemplate.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs/promises";
+import { TAG_TEMPLATE } from "./utils/constants.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,94 +13,98 @@ const __dirname = path.dirname(__filename);
  * @returns {Promise<void>} A promise that resolves when the template creation is complete.
  * @throws {Error} If there's an issue creating the template.
  */
-async function createTemplate(title) {
-  const projectRoot = path.join(__dirname, "..");
-  const templateDir = path.join(projectRoot, "templates");
-  const srcDir = path.join(projectRoot, "src");
+async function createTemplate(title, tagName) {
+	const projectRoot = path.join(__dirname, "..");
+	const templateDir = path.join(projectRoot, "templates");
+	const srcDir = path.join(projectRoot, "src");
 
-  const mdxsDir = path.join(srcDir, "content/animation/");
-  const animationsDir = path.join(srcDir, "animations");
+	const mdxsDir = path.join(srcDir, "content/animation/");
+	const animationsDir = path.join(srcDir, "animations");
 
-  // TODO: get file length based on mdx
-  try {
-    const files = await fs.readdir(mdxsDir);
-    const idx = files.length + 1;
+	// TODO: get file length based on mdx
+	try {
+		const files = await fs.readdir(mdxsDir);
+		const idx = files.length + 1;
 
-    const templateAnimationDir = path.join(templateDir, "animation");
-    const templateMdxDir = path.join(templateDir, "mdx");
+		const templateAnimationDir = path.join(templateDir, "animation");
+		const templateMdxDir = path.join(templateDir, "mdx");
 
-    const animationTargetDir = path.join(animationsDir, `animation${idx}`);
-    await createFolder(animationTargetDir);
+		const animationTargetDir = path.join(animationsDir, `animation${idx}`);
+		await createFolder(animationTargetDir);
 
-    await writeContents(
-      templateAnimationDir,
-      animationTargetDir,
-      undefined,
-      title,
-      idx,
-    );
+		await writeContents(
+			templateAnimationDir,
+			animationTargetDir,
+			undefined,
+			title,
+			idx,
+			tagName
+		);
 
-    const mdxTargetName = `post-${idx}.mdx`;
-    await writeContents(templateMdxDir, mdxsDir, mdxTargetName, title, idx);
-  } catch (error) {
-    console.error("Error generating Templates :", error);
-  }
+		const mdxTargetName = `post-${idx}.mdx`;
+		await writeContents(templateMdxDir, mdxsDir, mdxTargetName, title, idx, tagName);
+	} catch (error) {
+		console.error("Error generating Templates :", error);
+	}
 }
 
 async function createFolder(folderPath) {
-  try {
-    await fs.mkdir(folderPath, { recursive: true });
-    console.log(`Folder created successfully at ${folderPath}`);
-  } catch (error) {
-    console.error("Error creating Folder :", error);
-  }
+	try {
+		await fs.mkdir(folderPath, { recursive: true });
+		console.log(`Folder created successfully at ${folderPath}`);
+	} catch (error) {
+		console.error("Error creating Folder :", error);
+	}
 }
 
 async function createFile(filePath, content = "") {
-  try {
-    await fs.writeFile(filePath, content);
-    console.log(`File created successfully at ${filePath}`);
-  } catch (error) {
-    console.error("Error creating file:", error);
-  }
+	try {
+		await fs.writeFile(filePath, content);
+		console.log(`File created successfully at ${filePath}`);
+	} catch (error) {
+		console.error("Error creating file:", error);
+	}
 }
 
 async function readFileContent(filePath) {
-  try {
-    const content = await fs.readFile(filePath, "utf8");
-    console.log("File content:", content);
-    return content;
-  } catch (error) {
-    console.error("Error reading file:", error);
-  }
+	try {
+		const content = await fs.readFile(filePath, "utf8");
+		console.log("File content:", content);
+		return content;
+	} catch (error) {
+		console.error("Error reading file:", error);
+	}
 }
 
-async function writeContents(dir, target, targetName, title, slug) {
-  // NOTE: read contents
-  const tempFiles = await fs.readdir(dir);
-  const promises = tempFiles.map((filename) => {
-    const dest = path.join(dir, filename);
-    return readFileContent(dest);
-  });
-  const result = await Promise.all(promises);
-  const contents = result.flat();
+async function writeContents(dir, target, targetName, title, slug, tagName = "") {
+	// NOTE: read contents
+	const tempFiles = await fs.readdir(dir);
+	const promises = tempFiles.map((filename) => {
+		const dest = path.join(dir, filename);
+		return readFileContent(dest);
+	});
+	const result = await Promise.all(promises);
+	const contents = result.flat();
 
-  // NOTE: write contents
-  const writePromise = tempFiles.map((filename, index) => {
-    const dest = path.join(target, targetName ?? filename);
-    let content = contents[index];
-    content = content.replaceAll("{{number}}", slug);
-    content = content.replaceAll("{{title}}", title);
-    return createFile(dest, content);
-  });
+	// NOTE: write contents
+	const writePromise = tempFiles.map((filename, index) => {
+		const dest = path.join(target, targetName ?? filename);
+		let content = contents[index];
+		content = content.replaceAll("{{number}}", slug);
+		content = content.replaceAll("{{title}}", title);
 
-  await Promise.all(writePromise);
+		const replacedTag = tagName ? `${TAG_TEMPLATE}: '${tagName}'` : ""
+		content = content.replaceAll("{{tag}}", replacedTag);
+		return createFile(dest, content);
+	});
+
+	await Promise.all(writePromise);
 }
 
 export {
-  createTemplate,
-  createFolder,
-  createFile,
-  readFileContent,
-  writeContents,
+	createTemplate,
+	createFolder,
+	createFile,
+	readFileContent,
+	writeContents,
 };
--- script/templateAction.js
+++ script/templateAction.js
@@ -2,16 +2,19 @@ import { program } from "commander";
 import { createTemplate } from "./createTemplate.js";
 
 program
-  .version("1.0.0")
-  .description("Create a new template file for Astro.js project");
+	.version("1.0.0")
+	.description("Create a new template file for Astro.js project");
 
-program.option("-n, --name <name>", "Animation name").parse(process.argv);
+program
+	.option("-n, --name <name>", "Animation name")
+	.option("-t, --tag <tag>", "Animation tag")
+	.parse(process.argv);
 
 const options = program.opts();
 
 if (options.name) {
-  createTemplate(options.name);
+	createTemplate(options.name, options?.tag);
 } else {
-  console.error("Please provide a name using the -n or --name option");
-  process.exit(1);
+	console.error("Please provide a name using the -n or --name option");
+	process.exit(1);
 }
